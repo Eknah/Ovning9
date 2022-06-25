b5c601370d583b34cb87ae96504b7e6428bac999
Eknah <eknah@protonmail.com>
2022-06-25 16:54:23 +0200

    Start implement ToDo-list functionality

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,3 +1,14 @@
 
+let tbLabel = document.querySelector("#tbLabel");
 let btnAdd = document.querySelector("#btnAdd");
+let listContent = document.querySelector("#listContent");
 
+const addItem = function() {
+    let newDiv = document.createElement('div');
+    newDiv.innerText = tbLabel.value;
+    newDiv.classList.add('listItem');
+    newDiv.addEventListener('click', (e) => {e.target.classList.toggle('listItemChecked');})
+    listContent.append(newDiv);
+}
+
+btnAdd.addEventListener('click', addItem);
