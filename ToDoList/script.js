4c5824050837f8bc939a66cca39c47db0ccb3bd8
Eknah <eknah@protonmail.com>
2022-06-26 21:53:21 +0200

    Add functionality to remove entries

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -5,9 +5,26 @@ let listContent = document.querySelector("#listContent");
 
 const addItem = function() {
     let newDiv = document.createElement('div');
-    newDiv.innerText = tbLabel.value;
+    //newDiv.innerText = tbLabel.value;
     newDiv.classList.add('listItem');
-    newDiv.addEventListener('click', (e) => {e.target.classList.toggle('listItemChecked');})
+
+    let newDivLeft = document.createElement('div');
+    newDivLeft.innerText = tbLabel.value;
+    newDivLeft.classList.add('innerListItemChild');
+    newDivLeft.addEventListener('click', (e) => {e.target.classList.toggle('listItemChecked');})
+
+    let newDivRight = document.createElement('div');
+    newDivRight.classList.add('innerListItemChild');
+
+    let removeButton = document.createElement('button');
+    removeButton.innerHTML = 'Ta bort';
+    removeButton.addEventListener('click', (e) => {e.target.parentNode.parentNode.remove();})
+    
+    newDivRight.append(removeButton);
+
+    newDiv.append(newDivLeft);
+    newDiv.append(newDivRight);
+
     listContent.append(newDiv);
 }
 
